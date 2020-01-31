ab95eff4ba4136f7ea05bb9f756604a4e11f704a
shuaipengge <shuaipengge@gmail.com>
2020-01-31 22:47:07 +0800

    注释接口

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('博客API') //  标题
+    .setDescription('基于NestJS的博客API')  // 描述
+    .setVersion('1.0')
+    .addTag('NestJS','Blog restful api') // 添加标签
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document); //路径
+
   await app.listen(3000);
 }
 bootstrap();
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @Controller('posts')
+@ApiTags('帖子')
 export class PostsController {
     @Get()
+    @ApiOperation({summary:'显示博客列表'})
     index() {
         return [
             { id: 1 }
@@ -10,18 +13,19 @@ export class PostsController {
     }
 
     @Post()
-    create(){
+    @ApiOperation({summary:'创建博客'})
+    create() {
         return {
             success: true
         }
     }
 
     @Get(':id')
-    detail(){
+    detail() {
         return {
             success: true,
-            data:[
-                {id:1, title: 'hello'}
+            data: [
+                { id: 1, title: 'hello' }
             ]
         }
     }
