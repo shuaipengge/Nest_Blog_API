2deddbfeb4f101a2662ad8aca79d7d017c857e13
shuaipengge <shuaipengge@gmail.com>
2020-01-31 23:06:02 +0800

    处理POST请求 接口文档

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
+
+class CreatePostDto {
+    @ApiProperty({ description: '帖子标题' })
+    title: string
+    @ApiProperty({ description: '帖子内容' })
+    content: string
+    @ApiProperty({ description: '帖子标签' })
+    tag: string
+}
 
 @Controller('posts')
 @ApiTags('帖子')
 export class PostsController {
     @Get()
-    @ApiOperation({summary:'显示博客列表'})
+    @ApiOperation({ summary: '显示博客列表' })
     index() {
         return [
             { id: 1 }
@@ -13,20 +22,24 @@ export class PostsController {
     }
 
     @Post()
-    @ApiOperation({summary:'创建博客'})
-    create() {
-        return {
-            success: true
-        }
-    }
-
-    @Get(':id')
-    detail() {
+    @ApiOperation({ summary: '创建博客' })
+    // 参数装饰器
+    // @Query() Query, @Param() param
+    create(@Body() body: CreatePostDto) {
+        body
         return {
             success: true,
-            data: [
-                { id: 1, title: 'hello' }
-            ]
-        }
+            data:[{ id: 1, body: body }]
     }
+}
+
+@Get(':id')
+detail() {
+    return {
+        success: true,
+        data: [
+            { id: 1, title: 'hello' }
+        ]
+    }
+}
 }
