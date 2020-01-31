3e467ebf96e4f0d39e912d5e1a5baa958a30f98b
shuaipengge <shuaipengge@gmail.com>
2020-01-31 23:26:02 +0800

    修改 删除接口

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
 
 class CreatePostDto {
+    @ApiProperty({ description: '帖子标题' })
+    title: string
+    @ApiProperty({ description: '帖子内容' })
+    content: string
+    @ApiProperty({ description: '帖子标签' })
+    tag: string
+}
+
+class UpdatePostDto {
     @ApiProperty({ description: '帖子标题' })
     title: string
     @ApiProperty({ description: '帖子内容' })
@@ -14,7 +23,7 @@ class CreatePostDto {
 @ApiTags('帖子')
 export class PostsController {
     @Get()
-    @ApiOperation({ summary: '显示博客列表' })
+    @ApiOperation({ summary: '博客列表' })
     index() {
         return [
             { id: 1 }
@@ -26,20 +35,39 @@ export class PostsController {
     // 参数装饰器
     // @Query() Query, @Param() param
     create(@Body() body: CreatePostDto) {
-        body
         return {
             success: true,
-            data:[{ id: 1, body: body }]
+            data: [{ id: 1, body: body }]
+        }
     }
-}
 
-@Get(':id')
-detail() {
-    return {
-        success: true,
-        data: [
-            { id: 1, title: 'hello' }
-        ]
+    @Get(':id')
+    @ApiOperation({ summary: '博客详情' })
+    detail(@Param('id') id: string) {
+        return {
+            success: true,
+            data: [
+                { id: id, title: 'hello' }
+            ]
+        }
     }
-}
+
+    @Put(':id')
+    @ApiOperation({ summary: '修改博客' })
+    update(@Param('id') id: string, @Body() body: UpdatePostDto) {
+        return {
+            success: true,
+            data: [{ id: 1, body: body }]
+        }
+    }
+
+    @Delete(':id')
+    @ApiOperation({ summary: '删除博客' })
+    remove(@Param('id') id: string) {
+        return {
+            success: true,
+            data: [{ id: id }]
+        }
+    }
+
 }
